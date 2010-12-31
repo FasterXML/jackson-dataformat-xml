99c64dddfeb2b7888c3d2f01b3bf7068c8c33aa1
Tatu Saloranta <tsaloranta@gmail.com>
2010-12-30 22:00:43 -0800

    Move unit tests from jackson-xc package

---
+++ src/test/java/com/fasterxml/jackson/xml/TestXmlDeclaration.java
@@ -0,0 +1,30 @@
+package com.fasterxml.jackson.xml;
+
+import org.codehaus.jackson.xml.ToXmlGenerator;
+import org.codehaus.jackson.xml.XmlMapper;
+
+public class TestXmlDeclaration extends XmlTestBase
+{
+    /*
+    /**********************************************************
+    /* Unit tests
+    /**********************************************************
+     */
+    
+    public void testXml10Declaration() throws Exception
+    {
+        XmlMapper mapper = new XmlMapper();
+        mapper.configure(ToXmlGenerator.Feature.WRITE_XML_DECLARATION, true);
+        String xml = mapper.writeValueAsString(new StringBean("123"));
+        assertEquals(xml, "<?xml version='1.0' encoding='UTF-8'?><StringBean><text>123</text></StringBean>");
+    }
+
+    public void testXml11Declaration() throws Exception
+    {
+        XmlMapper mapper = new XmlMapper();
+        mapper.configure(ToXmlGenerator.Feature.WRITE_XML_1_1, true);
+        String xml = mapper.writeValueAsString(new StringBean("abcd"));
+        assertEquals(xml, "<?xml version='1.1' encoding='UTF-8'?><StringBean><text>abcd</text></StringBean>");
+    }
+    
+}
---
+++ src/test/java/com/fasterxml/jackson/xml/XmlTestBase.java
@@ -0,0 +1,228 @@
+package com.fasterxml.jackson.xml;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.JsonToken;
+
+import junit.framework.TestCase;
+
+public abstract class XmlTestBase
+    extends TestCase
+{
+    /*
+    /**********************************************************
+    /* Helper types
+    /**********************************************************
+     */
+
+    /**
+     * Sample class from Jackson tutorial ("JacksonInFiveMinutes")
+     */
+    protected static class FiveMinuteUser {
+        public enum Gender { MALE, FEMALE };
+
+        public static class Name
+        {
+          private String _first, _last;
+
+          public Name() { }
+          public Name(String f, String l) {
+              _first = f;
+              _last = l;
+          }
+          
+          public String getFirst() { return _first; }
+          public String getLast() { return _last; }
+
+          public void setFirst(String s) { _first = s; }
+          public void setLast(String s) { _last = s; }
+
+          @Override
+          public boolean equals(Object o)
+          {
+              if (o == this) return true;
+              if (o == null || o.getClass() != getClass()) return false;
+              Name other = (Name) o;
+              return _first.equals(other._first) && _last.equals(other._last); 
+          }
+        }
+
+        private Gender _gender;
+        private Name _name;
+        private boolean _isVerified;
+        private byte[] _userImage;
+
+        public FiveMinuteUser() { }
+
+        public FiveMinuteUser(String first, String last, boolean verified, Gender g, byte[] data)
+        {
+            _name = new Name(first, last);
+            _isVerified = verified;
+            _gender = g;
+            _userImage = data;
+        }
+        
+        public Name getName() { return _name; }
+        public boolean isVerified() { return _isVerified; }
+        public Gender getGender() { return _gender; }
+        public byte[] getUserImage() { return _userImage; }
+
+        public void setName(Name n) { _name = n; }
+        public void setVerified(boolean b) { _isVerified = b; }
+        public void setGender(Gender g) { _gender = g; }
+        public void setUserImage(byte[] b) { _userImage = b; }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (o == this) return true;
+            if (o == null || o.getClass() != getClass()) return false;
+            FiveMinuteUser other = (FiveMinuteUser) o;
+            if (_isVerified != other._isVerified) return false;
+            if (_gender != other._gender) return false; 
+            if (!_name.equals(other._name)) return false;
+            byte[] otherImage = other._userImage;
+            if (otherImage.length != _userImage.length) return false;
+            for (int i = 0, len = _userImage.length; i < len; ++i) {
+                if (_userImage[i] != otherImage[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    
+    protected static class StringBean
+    {
+        public String text;
+
+        public StringBean() { this("foobar"); }
+        public StringBean(String s) { text = s; }
+    }
+
+    /**
+     * Simple wrapper around String type, usually to test value
+     * conversions or wrapping
+     */
+    protected static class StringWrapper {
+        public String str;
+
+        public StringWrapper() { }
+        public StringWrapper(String value) {
+            str = value;
+        }
+    }
+
+    /*
+    /**********************************************************
+    /* Some sample documents:
+    /**********************************************************
+     */
+
+    protected final static int SAMPLE_SPEC_VALUE_WIDTH = 800;
+    protected final static int SAMPLE_SPEC_VALUE_HEIGHT = 600;
+    protected final static String SAMPLE_SPEC_VALUE_TITLE = "View from 15th Floor";
+    protected final static String SAMPLE_SPEC_VALUE_TN_URL = "http://www.example.com/image/481989943";
+    protected final static int SAMPLE_SPEC_VALUE_TN_HEIGHT = 125;
+    protected final static String SAMPLE_SPEC_VALUE_TN_WIDTH = "100";
+    protected final static int SAMPLE_SPEC_VALUE_TN_ID1 = 116;
+    protected final static int SAMPLE_SPEC_VALUE_TN_ID2 = 943;
+    protected final static int SAMPLE_SPEC_VALUE_TN_ID3 = 234;
+    protected final static int SAMPLE_SPEC_VALUE_TN_ID4 = 38793;
+
+    protected final static String SAMPLE_DOC_JSON_SPEC = 
+        "{\n"
+        +"  \"Image\" : {\n"
+        +"    \"Width\" : "+SAMPLE_SPEC_VALUE_WIDTH+",\n"
+        +"    \"Height\" : "+SAMPLE_SPEC_VALUE_HEIGHT+","
+        +"\"Title\" : \""+SAMPLE_SPEC_VALUE_TITLE+"\",\n"
+        +"    \"Thumbnail\" : {\n"
+        +"      \"Url\" : \""+SAMPLE_SPEC_VALUE_TN_URL+"\",\n"
+        +"\"Height\" : "+SAMPLE_SPEC_VALUE_TN_HEIGHT+",\n"
+        +"      \"Width\" : \""+SAMPLE_SPEC_VALUE_TN_WIDTH+"\"\n"
+        +"    },\n"
+        +"    \"IDs\" : ["+SAMPLE_SPEC_VALUE_TN_ID1+","+SAMPLE_SPEC_VALUE_TN_ID2+","+SAMPLE_SPEC_VALUE_TN_ID3+","+SAMPLE_SPEC_VALUE_TN_ID4+"]\n"
+        +"  }"
+        +"}"
+        ;
+    
+    /*
+    /**********************************************************
+    /* Construction
+    /**********************************************************
+     */
+
+    protected XmlTestBase() {
+        super();
+    }
+
+    /*
+    /**********************************************************
+    /* Additional assertion methods
+    /**********************************************************
+     */
+
+    protected void assertToken(JsonToken expToken, JsonToken actToken)
+    {
+        if (actToken != expToken) {
+            fail("Expected token "+expToken+", current token "+actToken);
+        }
+    }
+
+    protected void assertToken(JsonToken expToken, JsonParser jp)
+    {
+        assertToken(expToken, jp.getCurrentToken());
+    }
+
+    /**
+     * Method that gets textual contents of the current token using
+     * available methods, and ensures results are consistent, before
+     * returning them
+     */
+    protected String getAndVerifyText(JsonParser jp)
+        throws IOException, JsonParseException
+    {
+        // Ok, let's verify other accessors
+        int actLen = jp.getTextLength();
+        char[] ch = jp.getTextCharacters();
+        String str2 = new String(ch, jp.getTextOffset(), actLen);
+        String str = jp.getText();
+
+        if (str.length() !=  actLen) {
+            fail("Internal problem (jp.token == "+jp.getCurrentToken()+"): jp.getText().length() ['"+str+"'] == "+str.length()+"; jp.getTextLength() == "+actLen);
+        }
+        assertEquals("String access via getText(), getTextXxx() must be the same", str, str2);
+
+        return str;
+    }
+
+    protected void verifyFieldName(JsonParser jp, String expName)
+        throws IOException
+    {
+        assertEquals(expName, jp.getText());
+        assertEquals(expName, jp.getCurrentName());
+    }
+    
+    /*
+    /**********************************************************
+    /* Helper methods, other
+    /**********************************************************
+     */
+    
+    /**
+     * Helper method that tries to remove unnecessary namespace
+     * declaration that default JDK XML parser (SJSXP) seems fit
+     * to add.
+     */
+    protected static String removeSjsxpNamespace(String xml)
+    {
+        final String match = " xmlns=\"\"";
+        int ix = xml.indexOf(match);
+        if (ix > 0) {
+            xml = xml.substring(0, ix) + xml.substring(ix+match.length());
+        }
+        return xml;
+    }
+}
