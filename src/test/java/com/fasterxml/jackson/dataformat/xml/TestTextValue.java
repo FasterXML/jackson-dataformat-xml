f7fec8ea4cd47662f0bedc9f94d42e591766f0a6
Tatu Saloranta <tsaloranta@salesforce.com>
2012-04-10 11:20:11 -0700

    Add a unit test for Issue-24

--- src/test/java/com/fasterxml/jackson/dataformat/xml/TestTextValue.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/TestTextValue.java
@@ -2,6 +2,7 @@ package com.fasterxml.jackson.dataformat.xml;
 
 import java.io.IOException;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.*;
 
 public class TestTextValue extends XmlTestBase
@@ -23,7 +24,34 @@ public class TestTextValue extends XmlTestBase
 		public String text = "something";
 	}
 	
-    /*
+	// Issue-24:
+
+	static class Main {
+		@JsonProperty("com.test.stack") public Stack stack;
+	}
+	static class Stack {
+		public String name;
+
+		@JsonProperty("com.test.stack.slot")
+		public Slot slot;
+	}
+	static class Slot {
+		@JsonProperty("name")
+		public String name;
+
+		@JsonProperty("id")
+		public String id;
+
+		@JsonProperty("height")
+		public String height;
+
+		@JsonProperty("width")
+		public String width;
+
+		@JacksonXmlText
+		public String value;
+	}
+	/*
     /**********************************************************
     /* Unit tests
     /**********************************************************
@@ -43,6 +71,24 @@ public class TestTextValue extends XmlTestBase
     			Simple.class);
     	assertEquals(99, result.a);
     	assertEquals("else", result.text);
+    }
+    
+    public void testIssue24() throws Exception
+    {
+    	final String TEXT = "+/null/this is a long string";
+    	final String XML =
+    			"<main>\n"
+    			+"<com.test.stack name='stack1'>\n"
+    			+"<com.test.stack.slot height='0' id='0' name='slot0' width='0'>"
+    			+TEXT
+    			+"</com.test.stack.slot>\n"
+    			+"</com.test.stack>\n"
+    			+"</main>";
+    	XmlMapper mapper = new XmlMapper();
+    	Main main = mapper.readValue(XML, Main.class);
+    	assertNotNull(main.stack);
+    	assertNotNull(main.stack.slot);
+    	assertEquals(TEXT, main.stack.slot.value);
     }
     
     /* // Uncomment to see how JAXB works here:
