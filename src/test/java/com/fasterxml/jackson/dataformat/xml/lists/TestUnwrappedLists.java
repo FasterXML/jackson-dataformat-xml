3069602e873e46000163146dd284370ab86ad16c
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-02-13 21:35:31 -0800

    Add a unit test for #101

--- src/test/java/com/fasterxml/jackson/dataformat/xml/lists/TestUnwrappedLists.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/lists/TestUnwrappedLists.java
@@ -2,6 +2,7 @@ package com.fasterxml.jackson.dataformat.xml.lists;
 
 import java.util.*;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlTestBase;
 import com.fasterxml.jackson.dataformat.xml.annotation.*;
@@ -27,6 +28,32 @@ public class TestUnwrappedLists extends XmlTestBase
         public List<Optional> optional;
     } 
 
+    // For [Issue#101]
+    @JacksonXmlRootElement(localName = "root")    
+    @JsonPropertyOrder({ "unwrapped", "name" })
+    static class Root {
+        @JacksonXmlProperty(localName = "unwrapped")
+        @JacksonXmlElementWrapper(useWrapping = false)
+        public List<UnwrappedElement> unwrapped;
+
+        public String name;
+    }
+
+     static class UnwrappedElement {
+        public UnwrappedElement () {}
+
+        public UnwrappedElement (String id, String type) {
+            this.id = id;
+            this.type = type;
+        }
+
+        @JacksonXmlProperty(isAttribute = true)
+        public String id;
+
+        @JacksonXmlProperty(isAttribute = true)
+        public String type;
+    }
+
     /*
     /**********************************************************
     /* Unit tests
@@ -50,5 +77,30 @@ public class TestUnwrappedLists extends XmlTestBase
         Optional opt = ob.optional.get(0);
         assertEquals("123-456-7890", opt.number);
         assertEquals("NOT SET", opt.type);
+    }
+
+    // [Issue#101]
+    public void testWithTwoAttributes() throws Exception
+    {
+        final String EXP = "<root>"
+                +"<unwrapped id=\"1\" type=\"string\"/>"
+                +"<unwrapped id=\"2\" type=\"string\"/>"
+                +"<name>test</name>"
+                +"</root>";
+        Root rootOb = new Root();
+        rootOb.unwrapped = Arrays.asList(
+                new UnwrappedElement("1", "string"),
+                new UnwrappedElement("2", "string")
+        );
+        rootOb.name = "test";
+
+        // First, serialize, which works
+        String xml = MAPPER.writeValueAsString(rootOb);
+        assertEquals(EXP, xml);
+
+        // then try deserialize
+        Root result = MAPPER.readValue(xml, Root.class);
+        assertNotNull(result);
+        assertEquals(rootOb.name, result.name);
     }
 }
