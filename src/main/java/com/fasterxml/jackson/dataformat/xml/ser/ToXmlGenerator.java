db204d977749d6f0aae874e141bd7767738f0974
Cowtowncoder <tatu.saloranta@iki.fi>
2015-02-05 11:46:54 -0800

    Work on #136, add necessary check

--- src/main/java/com/fasterxml/jackson/dataformat/xml/ser/ToXmlGenerator.java
+++ src/main/java/com/fasterxml/jackson/dataformat/xml/ser/ToXmlGenerator.java
@@ -942,6 +942,10 @@ public final class ToXmlGenerator
     @Override
     public final JsonGenerator useDefaultPrettyPrinter()
     {
+        // related to [dataformat-xml#136], need to verify:
+        if (_cfgPrettyPrinter != null) {
+            return this;
+        }
         return setPrettyPrinter(new DefaultXmlPrettyPrinter());
     }
 
