24a10cedfe7a7db58121619626fc51b31cbe400a
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-04-21 14:13:44 -0700

    Add failing test for #191

---
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/failing/ListDeser191Test.java
@@ -0,0 +1,41 @@
+package com.fasterxml.jackson.dataformat.xml.failing;
+
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.dataformat.xml.*;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+public class ListDeser191Test extends XmlTestBase
+{
+    static class TestList {
+        @JacksonXmlElementWrapper(useWrapping = true, localName = "items")
+        @JacksonXmlProperty(localName = "item")
+        public ArrayList<ListItem> items;
+    }    
+
+    static class ListItem {
+        @JacksonXmlProperty(isAttribute = true)
+        public String name;
+    }    
+
+    public void testListDeser() throws Exception
+    {
+        ObjectMapper mapper = new XmlMapper();
+        final String XML =
+"<TestList>\n"+
+"    <items>\n"+
+"        <item name='Item1'/>\n"+
+"        <item name='Item2'>\n"+
+"        </item>\n"+
+"        <item name='Item3'/>\n"+
+"    </items>\n"+
+"</TestList>"
+                ;
+        TestList testList = mapper.readValue(XML, TestList.class);
+        assertNotNull(testList);
+        assertNotNull(testList.items);
+        assertEquals(3, testList.items.size());
+    }
+}
