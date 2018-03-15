8c7178513ed0ef9bd011ba87cf36ee43b9f90b12
Tatu Saloranta <tatu.saloranta@iki.fi>
2018-03-14 22:13:54 -0700

    changes wrt jackson-databind TypeResolverProvider

--- src/main/java/com/fasterxml/jackson/dataformat/xml/XmlTypeResolverProvider.java
+++ src/main/java/com/fasterxml/jackson/dataformat/xml/XmlTypeResolverProvider.java
@@ -1,16 +1,22 @@
 package com.fasterxml.jackson.dataformat.xml;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
 import com.fasterxml.jackson.databind.jsontype.TypeResolverProvider;
 import com.fasterxml.jackson.databind.jsontype.impl.StdTypeResolverBuilder;
 
+/**
+ * @since 3.0 (replacement for earlier AnnotationIntrospector-based approach in 2.x)
+ */
 public class XmlTypeResolverProvider
     extends TypeResolverProvider
 {
     private static final long serialVersionUID = 3L;
 
     @Override
-    protected StdTypeResolverBuilder _constructStdTypeResolverBuilder(JsonTypeInfo.Value typeInfo) {
+    protected StdTypeResolverBuilder _constructStdTypeResolverBuilder(MapperConfig<?> config,
+            JsonTypeInfo.Value typeInfo, JavaType baseType) {
         return new XmlTypeResolverBuilder(typeInfo);
     }
 }
