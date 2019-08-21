bf995c41053f62dfbdb8b5ae99ab628faad68b49
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-08-20 21:25:08 -0700

    fix wrt 2.10 databind

--- src/test/java/com/fasterxml/jackson/dataformat/xml/failing/DefaultTyping325Test.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/failing/DefaultTyping325Test.java
@@ -21,7 +21,7 @@ public class DefaultTyping325Test extends XmlTestBase
     public void testCanSerialize() throws IOException
     {
         ObjectMapper mapper = mapperBuilder()
-                .enableDefaultTyping(NoCheckSubTypeValidator.instance,
+                .activateDefaultTyping(NoCheckSubTypeValidator.instance,
                         ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.WRAPPER_OBJECT)
                 .build();
 
