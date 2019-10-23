e56ef8ce6308e4c297620de7cd5e046de2462a4d
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-10-23 10:32:21 -0700

    Fix an errant copy-paste line (where did that come from :) )

--- src/test/java/com/fasterxml/jackson/dataformat/xml/deser/XsiNil354Test.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/deser/XsiNil354Test.java
@@ -19,34 +19,13 @@ public class XsiNil354Test extends XmlTestBase
     public void testWithDoubleAsNull() throws Exception
     {
         DoubleWrapper bean = MAPPER.readValue(
-"<DoubleWrapper xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><\n" + 
-"    public void testDoesNotAffectHierarchy() throws Exception\n" + 
-"    {\n" + 
-"        String xml = \"<Parent xmlns:xsi=\\\"http://www.w3.org/2001/XMLSchema-instance\\\">\"\n" + 
-"                + \"<level1>\"\n" + 
-"                + \"<level2>\"\n" + 
-"                + \"<ignored xsi:nil=\\\"true\\\"/>\"\n" + 
-"                + \"<field>test-value</field>\"\n" + 
-"                + \"</level2>\"\n" + 
-"                + \"</level1>\"\n" + 
-"                + \"</Parent>\";\n" + 
-"        Parent bean = MAPPER.readValue(xml, Parent.class);\n" + 
-"\n" + 
-"        assertNotNull(bean);\n" + 
-"\n" + 
-"        // this should not be set, but having an xsi:nil field before it causes it to set the next field on the wrong class\n" + 
-"        assertEquals(\"test-value\", bean.level1.field);\n" + 
-"\n" + 
-"        // fails because field is set on level1 instead of on level2\n" + 
-"        assertEquals(\"test-value\", bean.level1.level2.field);\n" + 
-"    }\n" + 
-"d xsi:nil='true' /></DoubleWrapper>",
+"<DoubleWrapper xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'><d xsi:nil='true' /></DoubleWrapper>",
                 DoubleWrapper.class);
         assertNotNull(bean);
         assertNull(bean.d);
 
         bean = MAPPER.readValue(
-"<DoubleWrapper xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><d xsi:nil='true'>  </d></DoubleWrapper>",
+"<DoubleWrapper xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'><d xsi:nil='true'>  </d></DoubleWrapper>",
                 DoubleWrapper.class);
         assertNotNull(bean);
         assertNull(bean.d);
