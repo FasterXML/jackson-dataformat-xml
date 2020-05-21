612b2acaf5cc354a2ec6e0ad75f7029111df236e
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-05-20 21:56:37 -0700

    Implement #403 ("Make JsonNode work with XML")

---
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/deser/JsonNodeDeser403Test.java
@@ -0,0 +1,32 @@
+package com.fasterxml.jackson.dataformat.xml.deser;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.fasterxml.jackson.dataformat.xml.XmlTestBase;
+
+public class JsonNodeDeser403Test extends XmlTestBase
+{
+    final private ObjectMapper MAPPER = newMapper();
+
+    public void testSimpleNode() throws Exception
+    {
+        JsonNode root = MAPPER.readTree("<root attr='123' />");
+        assertTrue(root.isObject());
+        assertEquals(1, root.size());
+        assertEquals("123", root.get("attr").textValue());
+    }
+
+    // [dataformat-xml#403]: Allow sequences
+    public void testRepeated() throws Exception
+    {
+        JsonNode root = MAPPER.readTree("<root><value>a</value><value>b</value></root>");
+        assertTrue(root.isObject());
+        JsonNode arr = root.get("value");
+        assertEquals(JsonNodeType.ARRAY, arr.getNodeType());
+        assertTrue(arr.isArray());
+        assertEquals(2, arr.size());
+        assertEquals("a", root.at("/value/0").asText());
+        assertEquals("b", root.at("/value/1").asText());
+    }
+}
