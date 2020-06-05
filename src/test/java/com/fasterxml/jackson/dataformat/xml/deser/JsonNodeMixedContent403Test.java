c25683e2209daf9f3a506231e2d16db25fe8bea9
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-06-05 15:53:54 -0700

    Fixed #226

--- src/test/java/com/fasterxml/jackson/dataformat/xml/deser/JsonNodeMixedContent403Test.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/deser/JsonNodeMixedContent403Test.java
@@ -38,4 +38,16 @@ public class JsonNodeMixedContent403Test extends XmlTestBase
                 a2q("{'':['first','second','third'],'a':'1','b':'2'}")),
                 XML_MAPPER.readTree("<root>first<a>1</a>second<b>2</b>third</root>"));
     }
+
+    // [dataformat-xml#226]
+    public void testMixed226() throws Exception
+    {
+        final String XML = "<root>\n"
+                +"<a>mixed1 <b>leaf</b>"
+                +" mixed2</a>\n"
+                +"</root>";
+        assertEquals(JSON_MAPPER.readTree(
+                a2q("{'a':{'':['mixed1 ',' mixed2'],'b':'leaf'}}")),
+                XML_MAPPER.readTree(XML));
+    }
 }
