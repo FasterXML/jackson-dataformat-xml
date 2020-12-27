88790c2104a0243dd8eece33921e29c97f0e7b24
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-12-26 16:53:14 -0800

    minor javadoc fix

--- src/main/java/com/fasterxml/jackson/dataformat/xml/deser/FromXmlParser.java
+++ src/main/java/com/fasterxml/jackson/dataformat/xml/deser/FromXmlParser.java
@@ -55,7 +55,7 @@ public class FromXmlParser
     {
         /**
          * Feature that indicates whether XML Empty elements (ones where there are
-         * no separate start and end tages, but just one tag that ends with "/&gt;")
+         * no separate start and end tags, but just one tag that ends with "/&gt;")
          * are exposed as {@link JsonToken#VALUE_NULL}) or not. If they are not
          * returned as `null` tokens, they will be returned as {@link JsonToken#VALUE_STRING}
          * tokens with textual value of "" (empty String).
