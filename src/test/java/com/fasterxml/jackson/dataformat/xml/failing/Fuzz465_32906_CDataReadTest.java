c577dc031a15685f59e1137fb1c17eba6db71854
Tatu Saloranta <tatu.saloranta@iki.fi>
2021-04-05 10:00:46 -0700

    Minor test fix

--- src/test/java/com/fasterxml/jackson/dataformat/xml/failing/Fuzz465_32906_CDataReadTest.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/failing/Fuzz465_32906_CDataReadTest.java
@@ -1,5 +1,6 @@
 package com.fasterxml.jackson.dataformat.xml.failing;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlTestBase;
@@ -11,7 +12,11 @@ public class Fuzz465_32906_CDataReadTest extends XmlTestBase
     public void testIssue465() throws Exception
     {
         byte[] doc = readResource("/data/fuzz-32906.xml");
-        JsonNode root = MAPPER.readTree(doc);
-        assertNotNull(root);
+        try {
+            JsonNode root = MAPPER.readTree(doc);
+            fail("Should not pass, got: "+root);
+        } catch (StreamReadException e) {
+            verifyException(e, "Unexpected EOF in CDATA");
+        }
     }
 }
