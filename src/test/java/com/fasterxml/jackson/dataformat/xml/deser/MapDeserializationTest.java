8a8c2e51549ad455081e7b398df6b8fc8f53263b
Tatu Saloranta <tatu.saloranta@iki.fi>
2022-01-30 13:12:06 -0800

    Add a failing test for #498

--- src/test/java/com/fasterxml/jackson/dataformat/xml/deser/MapDeserializationTest.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/deser/MapDeserializationTest.java
@@ -1,5 +1,7 @@
 package com.fasterxml.jackson.dataformat.xml.deser;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
@@ -13,13 +15,13 @@ public class MapDeserializationTest extends XmlTestBase
     /**********************************************************
      */
 
-    private final XmlMapper MAPPER = newMapper();
+    private final XmlMapper XML_MAPPER = newMapper();
 
     // [dataformat-xml#14]
     public void testMapWithAttr() throws Exception
     {
         final String xml = "<order><person lang='en'>John Smith</person></order>";
-        Map<?,?> map = MAPPER.readValue(xml, Map.class);
+        Map<?,?> map = XML_MAPPER.readValue(xml, Map.class);
      
      // Will result in equivalent of:
      // { "person" : {
@@ -32,6 +34,10 @@ public class MapDeserializationTest extends XmlTestBase
      // we would just have '{ "person" : "John Smith" }'
      
          assertNotNull(map);
+         assertEquals(1, map.size());
+         Map<String,Object> inner = new LinkedHashMap<>();
+         inner.put("lang", "en");
+         inner.put("", "John Smith");
+         assertEquals(Collections.singletonMap("person", inner), map);
     }
-
 }
