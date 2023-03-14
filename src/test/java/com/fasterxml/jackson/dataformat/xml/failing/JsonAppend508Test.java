15da5adf85f0ae4880f7188a85c838c0eec07378
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-03-13 20:25:19 -0700

    Add a failing test for #508

---
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/failing/JsonAppend508Test.java
@@ -0,0 +1,58 @@
+package com.fasterxml.jackson.dataformat.xml.failing;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
+import com.fasterxml.jackson.databind.introspect.AnnotatedClass;
+import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
+import com.fasterxml.jackson.databind.ser.VirtualBeanPropertyWriter;
+import com.fasterxml.jackson.databind.util.Annotations;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlTestBase;
+
+public class JsonAppend508Test extends XmlTestBase
+{
+    // [dataformat-xml#508]: Duplication of virtual properties
+    @JsonAppend(props = @JsonAppend.Prop(name = "virtual", value = MyVirtualPropertyWriter.class))
+    public static class Pojo508 {
+        private final String name;
+
+        public Pojo508(String name) {
+            this.name = name;
+        }
+        public String getName() {
+            return name;
+        }
+    }
+
+    static class MyVirtualPropertyWriter extends VirtualBeanPropertyWriter {
+        private static final long serialVersionUID = 1L;
+
+        public MyVirtualPropertyWriter() {}
+
+        protected MyVirtualPropertyWriter(BeanPropertyDefinition propDef, Annotations contextAnnotations,
+                                          JavaType declaredType) {
+            super(propDef, contextAnnotations, declaredType);
+        }
+
+        @Override
+        protected Object value(Object bean, JsonGenerator g, SerializerProvider prov) {
+            return "bar";
+        }
+
+        @Override
+        public VirtualBeanPropertyWriter withConfig(MapperConfig<?> config, AnnotatedClass declaringClass,
+                BeanPropertyDefinition propDef, JavaType type) {
+            return new MyVirtualPropertyWriter(propDef, declaringClass.getAnnotations(), type);
+        }
+    }
+
+    private final XmlMapper MAPPER = newMapper();
+
+    // [dataformat-xml#508]: Duplication of virtual properties
+    public void testJsonAppend() throws Exception {
+        String xml = MAPPER.writeValueAsString(new Pojo508("foo"));
+        assertEquals("<Pojo><name>foo</name><virtual>bar</virtual></Pojo>",xml);
+    }
+}
