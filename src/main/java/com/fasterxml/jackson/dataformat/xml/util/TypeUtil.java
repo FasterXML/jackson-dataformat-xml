ffdbe537efb61aa71d675437c1edc04beedf08cb
joohyukkim <beanskobe@gmail.com>
2023-05-15 20:26:51 +0900

    Make class conditionals reusable

--- src/main/java/com/fasterxml/jackson/dataformat/xml/util/TypeUtil.java
+++ src/main/java/com/fasterxml/jackson/dataformat/xml/util/TypeUtil.java
@@ -15,10 +15,7 @@ public class TypeUtil
     public static boolean isIndexedType(JavaType type)
     {
         Class<?> cls = type.getRawClass();
-        if (type.isContainerType()
-            || Iterator.class.isAssignableFrom(cls)
-            || Stream.class.isAssignableFrom(cls)
-        ) {
+        if (type.isContainerType() || canHandleLikeAnIterable(cls)) {
             // One special case; byte[] will be serialized as base64-encoded String, not real array, so:
             // (actually, ditto for char[]; thought to be a String)
             if (cls == byte[].class || cls == char[].class) {
@@ -36,8 +33,17 @@ public class TypeUtil
 
     public static boolean isIndexedType(Class<?> cls)
     {
-        return  (cls.isArray() && cls != byte[].class && cls != char[].class) 
-            || Collection.class.isAssignableFrom(cls) || Iterator.class.isAssignableFrom(cls)
-            || Stream.class.isAssignableFrom(cls);
+        return  (cls.isArray() && cls != byte[].class && cls != char[].class)
+            || Collection.class.isAssignableFrom(cls) || canHandleLikeAnIterable(cls);
+    }
+
+    /**
+     * See <a href="https://github.com/FasterXML/jackson-dataformat-xml/pull/597">related disccussions</a> 
+     * for detailed history.
+     * 
+     * @since 2.16
+     */
+    private static boolean canHandleLikeAnIterable(Class<?> cls) {
+        return Iterator.class.isAssignableFrom(cls) || Stream.class.isAssignableFrom(cls);
     }
 }
