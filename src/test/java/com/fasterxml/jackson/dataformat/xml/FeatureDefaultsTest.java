536a3237d26b3092edbbf2a9c9e948ff3f71c632
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-05-29 13:44:59 -0700

    Fix a test

--- src/test/java/com/fasterxml/jackson/dataformat/xml/FeatureDefaultsTest.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/FeatureDefaultsTest.java
@@ -1,6 +1,7 @@
 package com.fasterxml.jackson.dataformat.xml;
 
 import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.xml.deser.FromXmlParser;
 import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 
@@ -16,7 +17,7 @@ public class FeatureDefaultsTest extends XmlTestBase
 
     public void testSerDefaults() throws Exception
     {
-        ObjectReader r = MAPPER.reader();
-        assertNotSame(r, r.with(ToXmlGenerator.Feature.WRITE_XML_1_1));
+        ObjectWriter w = MAPPER.writer();
+        assertNotSame(w, w.with(ToXmlGenerator.Feature.WRITE_XML_1_1));
     }
 }
