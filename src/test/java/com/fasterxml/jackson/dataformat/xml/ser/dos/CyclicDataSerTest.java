ba91d2b2ff21fe38de1d96bf529a7de4f20c1af5
PJ Fanning <pjfanning@users.noreply.github.com>
2023-07-08 13:31:15 +0100

    Update CyclicDataSerTest.java

--- src/test/java/com/fasterxml/jackson/dataformat/xml/ser/dos/CyclicDataSerTest.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/ser/dos/CyclicDataSerTest.java
@@ -1,5 +1,6 @@
 package com.fasterxml.jackson.dataformat.xml.ser.dos;
 
+import com.fasterxml.jackson.core.StreamWriteConstraints;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlTestBase;
@@ -22,8 +23,10 @@ public class CyclicDataSerTest extends XmlTestBase
             MAPPER.writeValueAsString(list);
             fail("expected JsonMappingException");
         } catch (JsonMappingException jmex) {
+            String exceptionPrefix = String.format("Document nesting depth (%d) exceeds the maximum allowed",
+                    StreamWriteConstraints.DEFAULT_MAX_DEPTH + 1);
             assertTrue("JsonMappingException message is as expected?",
-                    jmex.getMessage().startsWith("Document nesting depth (1001) exceeds the maximum allowed"));
+                    jmex.getMessage().startsWith(exceptionPrefix));
         }
     }
 }
