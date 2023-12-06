759bc766d8c852391edd350d89f6b17faac3ab3a
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-12-05 17:00:19 -0800

    Minor bit of test refactoring

--- src/test/java/com/fasterxml/jackson/dataformat/xml/fuzz/Fuzz618_64655_InvalidXMLTest.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/fuzz/Fuzz618_64655_InvalidXMLTest.java
@@ -1,54 +1,12 @@
 package com.fasterxml.jackson.dataformat.xml.fuzz;
 
-import com.fasterxml.jackson.annotation.JsonRootName;
-
 import com.fasterxml.jackson.core.exc.StreamReadException;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlTestBase;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText;
-
-import java.nio.file.*;
 
 public class Fuzz618_64655_InvalidXMLTest extends XmlTestBase
 {
-    static class AttributeBean
-    {
-        @JacksonXmlProperty(isAttribute=true, localName="attr")
-        public String text = "?";
-    }
-
-    static class Optional {
-        @JacksonXmlText
-        public String number = "NOT SET";
-        public String type = "NOT SET";
-    }
-
-    // [dataformat-xml#219]
-    static class Worker219
-    {
-        @JacksonXmlProperty(localName = "developer")
-        String developer;
-        @JacksonXmlProperty(localName = "tester")
-        String tester;
-        @JacksonXmlProperty(localName = "manager")
-        String manager;
-    }
-
-    // [dataformat-xml#219]
-    @JsonRootName("line")
-    static class Line219 {
-        public String code; //This should ideally be complex type
-        public String amount;
-    }
-
-    /*
-    /**********************************************************
-    /* Test methods
-    /**********************************************************
-     */
-
     private final XmlMapper MAPPER = newMapper();
 
     public void testWithInvalidXml1() throws Exception {
@@ -71,9 +29,9 @@ public class Fuzz618_64655_InvalidXMLTest extends XmlTestBase
 
     private void _testWithInvalidXml(int ix, String... errorToMatch) throws Exception
     {
+        byte[] doc = readResource("/data/fuzz-618-"+ix+".xml");
         try {
-            String path = "src/test/java/com/fasterxml/jackson/dataformat/xml/deser/invalid_xml_" + ix;
-            MAPPER.readTree(Files.readAllBytes(Paths.get(path)));
+            MAPPER.readTree(doc);
         } catch (StreamReadException e) {
             verifyException(e, errorToMatch);
         }
