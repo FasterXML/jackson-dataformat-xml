7e35baae9080b1ddcaf410ed8e425b9d4e2554c4
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-11-24 16:25:47 -0800

    Add test for #682 (passing with jackson-databind fix) (#685)

---
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/deser/EnumDeser682Test.java
@@ -0,0 +1,40 @@
+package com.fasterxml.jackson.dataformat.xml.deser;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.dataformat.xml.XmlTestBase;
+
+public class EnumDeser682Test extends XmlTestBase
+{
+    static enum Country {
+        ITALY("Italy"),
+        NETHERLANDS("Netherlands");
+
+        @JsonValue
+        final String value;
+
+        Country(String value) {
+            this.value = value;
+        }
+
+        @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+        public static Country fromValue(String value) {
+            for (Country b : Country.values()) {
+                if (b.value.equals(value)) {
+                    return b;
+                }
+            }
+            throw new IllegalArgumentException("Unexpected value '" + value + "'");
+        }
+    }
+
+    private final ObjectMapper MAPPER = newMapper();
+
+    public void testEnumDeser682() throws Exception {
+        String xml = MAPPER.writeValueAsString(Country.ITALY);
+        assertEquals("<Country>Italy</Country>", xml);
+        
+        assertEquals(Country.ITALY, MAPPER.readValue(xml, Country.class));
+    }
+}
