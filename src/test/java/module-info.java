b22e37d9dbe8f2507056fd2ce44c6f137679853c
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-01-11 16:04:50 -0800

    Work on #701, try to use `module-info.java` natively

---
+++ src/test/java/module-info.java
@@ -0,0 +1,30 @@
+//  Jackson 3.x module-info for jackson-dataformat-xml Test artifact
+module tools.jackson.dataformat.xml
+{
+    // First, same requires as the main artifact
+    
+    requires java.xml;
+    requires org.codehaus.stax2;
+
+    requires com.fasterxml.jackson.annotation;
+    requires tools.jackson.core;
+    requires tools.jackson.databind;
+
+    // Then test library dependencies
+    requires junit;
+    requires org.junit.jupiter.api;
+    requires org.junit.jupiter.params;
+    
+    // Then exports same as main artifact
+
+    exports tools.jackson.dataformat.xml;
+    exports tools.jackson.dataformat.xml.annotation;
+    exports tools.jackson.dataformat.xml.deser;
+    exports tools.jackson.dataformat.xml.ser;
+    exports tools.jackson.dataformat.xml.util;
+
+    provides tools.jackson.core.TokenStreamFactory with
+        tools.jackson.dataformat.xml.XmlFactory;
+    provides tools.jackson.databind.ObjectMapper with
+        tools.jackson.dataformat.xml.XmlMapper;
+}
