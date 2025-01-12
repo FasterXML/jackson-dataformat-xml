10eb673725abe22255209a8c288b437357ffb494
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-01-11 16:35:50 -0800

    Complete work

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -30,10 +30,20 @@ module tools.jackson.dataformat.xml
     // And then additional "opens" access for tests not in packages of main
 
     opens tools.jackson.dataformat.xml.adapters;
+    opens tools.jackson.dataformat.xml.deser.builder;
+    opens tools.jackson.dataformat.xml.deser.convert;
+    opens tools.jackson.dataformat.xml.deser.creator;
     opens tools.jackson.dataformat.xml.dos;
+    opens tools.jackson.dataformat.xml.failing;
+    opens tools.jackson.dataformat.xml.failing.records;
     opens tools.jackson.dataformat.xml.fuzz;
     opens tools.jackson.dataformat.xml.jaxb;
+    opens tools.jackson.dataformat.xml.incr;
+    opens tools.jackson.dataformat.xml.jdk17;
     opens tools.jackson.dataformat.xml.lists;
+    opens tools.jackson.dataformat.xml.misc;
     opens tools.jackson.dataformat.xml.node;
+    opens tools.jackson.dataformat.xml.stream;
     opens tools.jackson.dataformat.xml.vld;
+    opens tools.jackson.dataformat.xml.woodstox;
 }
