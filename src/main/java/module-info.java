46ca656ea7d6cd3280bc6e058d5683e8eebcdb70
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-02-09 12:34:40 -0800

    Clean up module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,11 @@
 //  Jackson 3.x module-info for jackson-dataformat-xml Main artifact
 module tools.jackson.dataformat.xml
 {
-    requires transitive java.xml;
-    requires transitive org.codehaus.stax2; // stax2-api
+    requires java.xml;
+    requires org.codehaus.stax2; // stax2-api
 
     requires transitive com.fasterxml.jackson.annotation;
-    requires transitive tools.jackson.core;
+    requires tools.jackson.core;
     requires transitive tools.jackson.databind;
 
     exports tools.jackson.dataformat.xml;
