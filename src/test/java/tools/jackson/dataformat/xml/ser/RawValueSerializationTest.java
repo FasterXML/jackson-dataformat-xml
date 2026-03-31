f702e4dcada3416ed6ff0bb9b2d63e0f6e11c92c
Tatu Saloranta <tatu.saloranta@iki.fi>
2026-03-30 17:18:18 -0700

    Add passing test for #448

--- src/test/java/tools/jackson/dataformat/xml/ser/RawValueSerializationTest.java
+++ src/test/java/tools/jackson/dataformat/xml/ser/RawValueSerializationTest.java
@@ -7,16 +7,28 @@ import com.fasterxml.jackson.annotation.JsonRawValue;
 
 import tools.jackson.dataformat.xml.XmlMapper;
 import tools.jackson.dataformat.xml.XmlTestUtil;
+import tools.jackson.dataformat.xml.annotation.JacksonXmlText;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// [dataformat-xml#269]
+// [dataformat-xml#269], [dataformat-xml#448]
 public class RawValueSerializationTest extends XmlTestUtil
 {
+    // [dataformat-xml#269]
     @JsonPropertyOrder({ "id", "raw" })
     public static class RawWrapper {
         public int id = 42;
 
+        @JsonRawValue
+        public String raw = "Mixed <b>content</b>";
+    }
+
+    // [dataformat-xml#448]
+    @JsonPropertyOrder({ "id", "raw" })
+    public static class RawWrapperWithXmlText {
+        public int id = 42;
+
+        @JacksonXmlText
         @JsonRawValue
         public String raw = "Mixed <b>content</b>";
     }
@@ -30,12 +42,22 @@ public class RawValueSerializationTest extends XmlTestUtil
     private final XmlMapper MAPPER = newMapper();
 
     @Test
-    void testRawValueSerialization() throws Exception
+    void testRawValueSerializationRegular() throws Exception
     {
         assertEquals("<RawWrapper>"
                 +"<id>42</id>"
                 +"<raw>Mixed <b>content</b></raw>"
                 +"</RawWrapper>",
                 MAPPER.writeValueAsString(new RawWrapper()).trim());
+    }
+
+    @Test
+    void testRawValueSerializationWithoutWrapping() throws Exception
+    {
+        assertEquals("<RawWrapperWithXmlText>"
+                +"<id>42</id>"
+                +"Mixed <b>content</b>"
+                +"</RawWrapperWithXmlText>",
+                MAPPER.writeValueAsString(new RawWrapperWithXmlText()).trim());
     }
 }
