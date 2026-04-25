ab652dbc20bbc61ed84ee06e670c4066e81e3cdd
Tatu Saloranta <tatu.saloranta@iki.fi>
2026-04-25 10:50:46 -0700

    ...

--- src/test/java/tools/jackson/dataformat/xml/tofix/MultidimArray556Test.java
+++ src/test/java/tools/jackson/dataformat/xml/tofix/MultidimArray556Test.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import tools.jackson.core.exc.StreamWriteException;
+import tools.jackson.databind.DatabindException;
 import tools.jackson.dataformat.xml.XmlMapper;
 import tools.jackson.dataformat.xml.XmlTestUtil;
 import tools.jackson.dataformat.xml.XmlWriteFeature;
@@ -30,7 +30,7 @@ public class MultidimArray556Test extends XmlTestUtil
         try {
             MAPPER.writeValueAsString(new boolean[][] { { true }, { false } });
             fail("Should not pass: nested arrays must be rejected");
-        } catch (StreamWriteException e) {
+        } catch (DatabindException e) {
             verifyException(e, "does not support nested arrays");
         }
     }
@@ -44,7 +44,7 @@ public class MultidimArray556Test extends XmlTestUtil
         try {
             MAPPER.writeValueAsString(nested);
             fail("Should not pass: nested Collections must be rejected");
-        } catch (StreamWriteException e) {
+        } catch (DatabindException e) {
             verifyException(e, "does not support nested arrays");
         }
     }
